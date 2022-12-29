6fe0ea2ebe6fc25bf2886b75f366ef9edba0b52d
Lopoland <darth_vader_192@yahoo.com>
2022-12-29 12:33:05 -0300

    JS 0.01

---
+++ js/index.js
@@ -0,0 +1,29 @@
+const numeroEdad = parseInt(prompt("ENTER AGE"));
+
+function accessDenied(accessDenied) {
+    return alert("ACCESS DENIED")
+}
+
+if (numeroEdad < "18") {
+    accessDenied()
+    window.location.href = "../images/accesdenied.webp"
+}
+else {
+    alert("ACCESS GRANTED")
+    let listaDeNombres = "";
+    let nombreIngresado = prompt("ENTER EVERY SOLDIER NAME, TYPE STOP WHEN DONE")
+
+    while (nombreIngresado !== "STOP") {
+        listaDeNombres = listaDeNombres + "\n" + nombreIngresado;
+        nombreIngresado = prompt("ENTER EVERY SOLDIER NAME, TYPE STOP WHEN DONE")
+    }
+
+    alert("THIS SOLDIERS HAVE BEEN REGISTRED" + listaDeNombres)
+
+}
+
+
+
+
+
+

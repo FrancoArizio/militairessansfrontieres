f8aeb50f7c7214d2c579cb24b94a60efa3b2e050
Franco Arizio <darth_vader_192@yahoo.com>
2023-03-09 14:51:09 -0300

    FIX 0.02

--- js/index.js
+++ js/index.js
@@ -13,7 +13,6 @@ formulario.addEventListener('submit', (e) =>{
         return
     } 
     else {
-        /* window.location.href = "../main.html" */
         window.location.href = "/main.html"
     }
 });
